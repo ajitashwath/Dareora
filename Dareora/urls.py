8d4ac1ddf0132a5749e2740751f24226949871c9
VishalVerma249 <vishal098verma@gmail.com>
2025-07-21 20:24:22 +0530

    inital folder pushed

---
+++ Dareora/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include("home.urls")),
+    path("explore/",include("explore.urls")),
+    path("leaderboard/",include("leaderboard.urls")),
+    path("community/",include("community.urls"))
+]
